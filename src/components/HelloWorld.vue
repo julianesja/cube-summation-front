0853e5e950934841636a7d2cdbf1161709b09044
julianesja <julianesja@gmail.com>
2019-09-23 12:44:46 -0500

    se crea la configuracion de axios

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,81 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="page-text-fields">
+    <v-container grid-list-xl fluid>
+      <v-layout row wrap>
+        <v-flex lg12>
+          <div slot="widget-content">
+            <v-form ref="form" v-model="valid">
+              <v-container>
+                <v-row row justify="center">
+                  <v-flex xs6>
+                    <v-text-field
+                      label="Casos de pruebas"
+                      v-model="model.testCases"
+                      :rules="rulesForm.testCases"
+                      type="number"
+                    ></v-text-field>
+                  </v-flex>
+                  <v-flex xs2>
+                    <div class="text-center">
+                      <v-btn @click="CreateTestCases">Crear</v-btn>
+                    </div>
+                  </v-flex>
+                </v-row>
+                <v-row
+                  row
+                  justify="center"
+                  v-for="iteration in model.iterations"
+                  v-bind:key="iteration.id"
+                >
+                  <v-flex xs6>
+                    <v-text-field label="Cubo" v-model="iteration.text" type="number"></v-text-field>
+                  </v-flex>
+                  <v-flex xs2>
+                    <div class="text-center">
+                      <v-btn>Crear Cubo</v-btn>
+                    </div>
+                  </v-flex>
+                </v-row>
+              </v-container>
+            </v-form>
+          </div>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data: () => ({
+    valid: false,
+    model: {
+      testCases: 0,
+      iterations: []
+    },
+    rulesForm: {
+      testCases: [
+        v => !!v || "El campo caso de prueba es requerido",
+        v => !parseInt(v) <= 0 || "El campo caso de prueba debe ser mayor a 0",
+        v =>
+          !(parseInt(v) > 50) ||
+          "El campo caso de prueba debe ser menor o igual a 50"
+      ]
+    }
+  }),
+  methods: {
+    CreateTestCases() {
+      this.model.iterations = [];
+      for (let i = 0; i < this.model.testCases; i++) {
+        this.model.iterations.push({
+          id: `i${i}`,
+          text: "",
+          operations: 0,
+          lenght: 0
+        });
+      }
+      
+    }
   }
-}
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
